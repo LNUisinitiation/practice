f4ea64401880846390e80fdb5418728eb95c37c0
LNUisinitiation <1589810996@qq.com>
2021-03-23 22:18:11 +0800

    eclipse报错：eclipse failed to init ct.sym

---
+++ phone.java
@@ -0,0 +1,1 @@
+The project was not built due to "Failed to init ct.sym for C:\Users\l'y'm\AppData\Local\Temp\eoi2674.tmp\plugins\org.eclipse.justj.openjdk.hotspot.jre.minimal.stripped.win32.x86_64_15.0.1.v20201027-0507\jre\lib\jrt-fs.jar". Fix the problem, then try refreshing this project and building it since it may be inconsistent
