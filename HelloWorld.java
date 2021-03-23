ffea855261160f60bee7020c3c89bf5ce7075772
LNUisinitiation <1589810996@qq.com>
2021-03-23 15:31:49 +0800

    这三个文件是为了练习使用git工具

---
+++ HelloWorld.java
@@ -0,0 +1,60 @@
+/*public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld");	
+	}
+}
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld");
+	}
+}
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld");
+	}
+}
+
+
+public class HelloWorld{
+	public static void main (String[] args){
+		System.out.println("ni hao liao ning da xue");
+	}
+}
+
+
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("ni hao zhu tou ge!!!");
+	}
+}
+
+
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld!!!");
+	}
+}
+
+
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld");
+	}
+}
+
+*/
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.print("liao ning da xue\n");
+		System.out.print("ni hao ");
+	}
+}
+
+
+
---
+++ java练习.java
@@ -0,0 +1,15 @@
+*/
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.print("liao ning da xue\n");
+		System.out.print("ni hao ");
+	}
+}
+*/
+
+public class HelloWorld{
+           public static void main(String[] args){
+                    System.out.print("liao ning da xue\n");
+	    System.out.println("ni hao liao ning da xue");	                  
+           }
+}
