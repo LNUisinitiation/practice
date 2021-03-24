35ab1716177f4cacfff70dd1fe6b5ab96825d372
LNUisinitiation <1589810996@qq.com>
2021-03-24 08:48:34 +0800

    每日练习编写java

---
+++ HellWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld!");
+	}
+
+} 
+
+
+/*
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.print("liao ning da xue\n");
+		System.out.print("ni hao ");
+	}
+}
+
+
+
+*/
