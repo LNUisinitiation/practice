8e3e70a64d43f476c5e44c68cc749682ef448110
LNUisinitiation <1589810996@qq.com>
2021-03-25 22:39:08 +0800

    20210325---java练习

---
+++ 20210325/HelloWorld.java
@@ -0,0 +1,70 @@
+//package JAVA_eclipse;
+//
+//public class HelloWorld {
+//	public static void main(String[] args) {
+//		System.out.println("你好");
+//	}
+//
+//}
+
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.print("nihao \n");
+//		System.out.println("nihao liaoningdaxue");
+//		System.out.print("LNU\n");
+//		System.out.println("1+1="+(1+1));
+//		System.out.println("我正在学习JAVA");
+//		System.out.println("我喜欢吃披萨");
+//		System.out.println("四级考试母题汇编");
+//		System.out.print("2*5="+(2*5));
+//	}
+//}
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.println("2021/03/25，练习写java程序。");
+//		System.out.println("3+6="+(3+6));
+//		System.out.println("10*10="+(10*10));
+//		System.out.println("这里是辽宁大学"+"LNU");
+//		System.out.println("这里是辽宁大学+LNU");
+//		System.out.println("六级考试"+"6级");
+//		System.out.println("联想小新air14 2020款 锐龙版");
+//	}
+//}
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.println("桌子上面有蓝牙耳机、计算器、笔记本、鼠标、充电线、"
+//				+ "键盘、水杯、水壶、书、橘子、口罩、糖...");
+//	}
+//}
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.println("明天有java课");
+//	}
+//}
+package JAVA_eclipse;
+public class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("明天有一节java课和一节数据库");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
