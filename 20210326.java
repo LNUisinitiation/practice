81c2949258b89c9ccf1890252035eaa421609502
LNUisinitiation <1589810996@qq.com>
2021-03-26 22:20:38 +0800

    将eclipse的字体增大，字符编码方式更改为UTF-8

---
+++ 20210326.java
@@ -0,0 +1,91 @@
+//package JAVA_eclipse;
+//
+//public class HelloWorld {
+//	public static void main(String[] args) {
+//		System.out.println("���");
+//	}
+//
+//}
+
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.print("nihao \n");
+//		System.out.println("nihao liaoningdaxue");
+//		System.out.print("LNU\n");
+//		System.out.println("1+1="+(1+1));
+//		System.out.println("������ѧϰJAVA");
+//		System.out.println("��ϲ��������");
+//		System.out.println("�ļ�����ĸ����");
+//		System.out.print("2*5="+(2*5));
+//	}
+//}
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.println("2021/03/25����ϰдjava����");
+//		System.out.println("3+6="+(3+6));
+//		System.out.println("10*10="+(10*10));
+//		System.out.println("������������ѧ"+"LNU");
+//		System.out.println("������������ѧ+LNU");
+//		System.out.println("��������"+"6��");
+//		System.out.println("����С��air14 2020�� ������");
+//	}
+//}
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.println("�����������������������������ʼǱ�����ꡢ����ߡ�"
+//				+ "���̡�ˮ����ˮ�����顢���ӡ����֡���...");
+//	}
+//}
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.println("������java��");
+//	}
+//}
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.println("������һ��java�κ�һ�����ݿ�");
+//	}
+//}
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.println("�й��ѧϰjava");
+//	}
+//}
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.println("�������й�");
+//	}
+//}
+//package JAVA_eclipse;
+//public class HelloWorld{
+//	public static void main(String[] args) {
+//		System.out.println("这里是中国");
+//	}
+//}
+package JAVA_eclipse;
+public class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("ni hao liao ning da xue");
+		System.out.println("100*100="+(100*100));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
