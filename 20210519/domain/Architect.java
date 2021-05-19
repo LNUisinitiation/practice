5e85f6bcf5c2b6308ed913da45f46245da9c66f5
LNUisinitiation <1589810996@qq.com>
2021-05-19 23:04:20 +0800

    20210519周三---java项目练习

---
+++ 20210519/domain/Architect.java
@@ -0,0 +1,24 @@
+package com.lnu.team.domain;
+
+public class Architect extends Designer {
+
+	private int stock;// 股票
+
+	public Architect() {
+		super();
+	}
+
+	public Architect(int id, String name, int age, double salary, Equipment equipment, double bonus, int stock) {
+		super(id, name, age, salary, equipment, bonus);
+		this.stock = stock;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+}
